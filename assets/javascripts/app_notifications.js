dc71bccab2534d0faec8e34d5e3a66f49f2bb1a6
Xiaoli Wang <nsnq@qq.com>
2018-08-29 16:11:37 +0800

    Add mark as seen/unseen without page jumping

--- assets/javascripts/app_notifications.js
+++ assets/javascripts/app_notifications.js
@@ -1,3 +1,20 @@
+function queryStringUrlReplacement(url, param, value) 
+{
+    var re = new RegExp("[\\?&]" + param + "=([^&#]*)"), match = re.exec(url), delimiter, newString;
+
+    if (match === null) {
+        // append new param
+        var hasQuestionMark = /\?/.test(url); 
+        delimiter = hasQuestionMark ? "&" : "?";
+        newString = url + delimiter + param + "=" + value;
+    } else {
+        delimiter = match[0].charAt(0);
+        newString = url.replace(re, delimiter + param + "=" + value);
+    }
+  
+    return newString;
+}
+
 $(document).ready(function()
 {
 	$("#notificationsLink").click(function()
@@ -32,52 +49,65 @@ $(document).ready(function()
 	$(".view-notification").click(function()
 	{
 		var link = $( this );
+
+		$.ajax({
+        	    type: "GET",
+        	    url: $(this).attr("href"),
+        	    dataType: 'html',
+        	    success: function() {
+        		//link.parent().removeClass( "new" );
+        		//link.remove();
+		    }
+                });
                 
                 // single notification toggle
                 if (link.parent().hasClass('notification')) {
                     if (link.parent().hasClass('new')) {
         	        link.parent().removeClass("new");
+                        link.attr('href', queryStringUrlReplacement(link.attr('href'), 'mark_as_unseen', 1));
+                        link.text(locale_str_mark_as_unseen).fadeIn();
                     } else {
         	        link.parent().addClass("new");
+                        link.attr('href', queryStringUrlReplacement(link.attr('href'), 'mark_as_unseen', ''));
+                        link.text(locale_str_mark_as_seen).fadeIn();
                     }
                 }
                 // group notices toogle
                 else {
                     if (link.parent().parent().parent().hasClass('new')) {
-        	        link.parent().parent().parent().removeClass("new");
+                        group_parent = link.parent().parent().parent()
+        	        group_parent.removeClass("new");
+                        all_links = group_parent.find('.view-notification')
+                        all_links.attr('href', queryStringUrlReplacement(all_links.attr('href'), 'mark_as_unseen', 1));
+                        all_links.text(locale_str_mark_as_unseen).fadeIn();
                     } else {
         	        link.parent().addClass("notification new"); // Mark as seen only for single notification
+                        link.attr('href', queryStringUrlReplacement(link.attr('href'), 'mark_as_unseen', ''));
+                        link.text(locale_str_mark_as_seen).fadeIn();
                     }
                 }
-        	link.remove();
-		$.ajax({
-        	type: "GET",
-        	url: $(this).attr("href"),
-        	dataType: 'html',
-        	success: function() {
-        		//link.parent().removeClass( "new" );
-        		//link.remove();
-			}
-        });
+
 		return false;
 	});
 
         $.ajax({
-        type: "GET",
-        url: "/app-notifications/unread-number/",
-        dataType: 'json',
-        success: function(data) {
-                     $("#notification_count").text(data);
-                     $("#notification_count").show();
-                 }
+            type: "GET",
+            url: "/app-notifications/unread-number/",
+            dataType: 'json',
+            success: function(data) {
+                $("#notification_count").text(data);
+                $("#notification_count").show();
+            }
         });
+
         $("#notification_count").click(function()
         {
-                window.open("/app-notifications/", "_blank");
+            window.open("/app-notifications/", "_blank");
         });
+
         $("#notification_count").hover(function()
         {
-                $(this).css("cursor","pointer");
+            $(this).css("cursor","pointer");
         });
 
 });
